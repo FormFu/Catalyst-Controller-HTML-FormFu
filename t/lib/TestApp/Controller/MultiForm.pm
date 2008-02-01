a7d2460a1e5d2ae7508417a5223e379e2132a36e
fireartist <fireartist@gmail.com>
2008-02-01 10:41:22 +0000

    test MultiFormMethod
    git-svn-id: file:///home/cafranks/github/googlecode/trunk/Catalyst-Controller-HTML-FormFu@857 3452f2c1-2087-41cb-87be-0eefa6d42935

--- t/lib/TestApp/Controller/MultiForm.pm
+++ t/lib/TestApp/Controller/MultiForm.pm
@@ -26,6 +26,40 @@ sub formconfig : Chained('multiform') : Args(0) : MultiFormConfig {
     }
 }
 
-sub end : ActionClass('RenderView') {}
+sub formmethod : Chained('multiform') : Args(0) : MultiFormMethod('_load_form') {
+    my ( $self, $c ) = @_;
+
+    my $multi = $c->stash->{multiform};
+
+    if ( $multi->complete ) {
+        my $params = $multi->current_form->params;
+
+        $c->stash->{results} = join "\n", map {
+            sprintf "%s: %s", $_, $params->{$_}
+        } keys %$params;
+
+        $c->stash->{message} = 'Complete';
+    }
+}
+
+sub _load_form : Private {
+    return {
+        id => 'formmethod',
+        params_ignore_underscore => 1,
+
+        crypt_args => {
+            '-key' => 'my secret',
+        },
+
+        forms => [
+            {
+                element => { name => 'page1' },
+            },
+            {
+                element => { name => 'page2' },
+            }
+        ],
+    };
+}
 
 1;
