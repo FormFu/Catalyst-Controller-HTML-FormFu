1304f447df83f019846e6a378528f5b182b2e16d
fireartist <fireartist@gmail.com>
2010-02-22 10:46:23 +0000

    fix POD link (RT #54836). wrap all POD lines to 80 cols
    git-svn-id: file:///home/cafranks/github/googlecode/trunk/Catalyst-Controller-HTML-FormFu@1674 3452f2c1-2087-41cb-87be-0eefa6d42935

--- lib/HTML/FormFu/Element/RequestToken.pm
+++ lib/HTML/FormFu/Element/RequestToken.pm
@@ -116,7 +116,8 @@ __END__
 
 =head1 NAME
 
-HTML::FormFu::Element::RequestToken - Hidden text field which contains a unique token
+HTML::FormFu::Element::RequestToken - Hidden text field which contains a unique 
+token
 
 =head1 SYNOPSIS
 
@@ -126,14 +127,18 @@ HTML::FormFu::Element::RequestToken - Hidden text field which contains a unique 
 
 =head1 DESCRIPTION
 
-This field can prevent CSRF attacks. It contains a random token. After submission the token is checked with the token which is stored in the session of the current user.
-See L<Catalyst::Controller::HTML::FormFu/USING TOKENS> for a convenient way how to use it.
+This field can prevent CSRF attacks. It contains a random token. After 
+submission the token is checked with the token which is stored in the session 
+of the current user.
+See L<Catalyst::Controller::HTML::FormFu/"request_token_enable"> for a 
+convenient way how to use it.
 
 =head1 ATTRIBUTES
 
 =head2 context
 
-Value of the stash key for the Catalyst context object (C<< $c >>). Defaults to C<context>.
+Value of the stash key for the Catalyst context object (C<< $c >>). 
+Defaults to C<context>.
 
 =head2 expiration_time
 
@@ -155,11 +160,13 @@ Generates a new token and stores it in the stash.
 
 =head2 remove_token
 
-Removes a specific token from the session. Returns C<1> if the key was found. C<0> otherwise.
+Removes a specific token from the session. Returns C<1> if the key was found. 
+C<0> otherwise.
 
 =head2 verify_token
 
-Checks whether a given token is already in the session. If it exists it is removed and C<verify_token> returns C<1>. C<0> otherwise.
+Checks whether a given token is already in the session. If it exists it is 
+removed and C<verify_token> returns C<1>. C<0> otherwise.
 
 =head1 SEE ALSO
 
